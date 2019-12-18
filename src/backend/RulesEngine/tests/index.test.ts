0a6ddd663f6b794436848aecf5581a0dd98fc380
Clark Bynum <cbynum@clearblade.com>
2019-12-18 16:30:01 -0600

    add list of unit tests for rules engine

---
+++ src/backend/RulesEngine/tests/index.test.ts
@@ -0,0 +1,10 @@
+// *** rule unit tests ***
+
+// timeframe.test
+// duration.test
+// AND test
+// OR test
+// nested AND
+// nested OR
+// test for created events with correct data
+// test that events fire correct actions
