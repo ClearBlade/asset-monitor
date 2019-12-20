58df266c74b89353dbae76aaf3dbf0038e3c2719
Clark Bynum <cbynum@clearblade.com>
2019-12-20 11:22:15 -0600

    more

--- src/backend/Util/index.ts
+++ src/backend/Util/index.ts
@@ -2,27 +2,13 @@ import { Assets } from '../collection-schema/Assets';
 import { NormalizerDeviceMap } from '../global-config';
 
 export interface FlattenedObject {
-    [key: string]: string | number | boolean | Array<any>;
+    [key: string]: string | number | boolean | Array<unknown>;
 }
-export function normalizeData(incomingData: any, normalizerConfig: NormalizerDeviceMap): Array<Assets> {
-    let dataToNormalize: Array<Record<string, any>> = [];
-    if (incomingData instanceof Array) {
-        dataToNormalize = incomingData;
-    } else if (typeof incomingData === 'object') {
-        dataToNormalize.push(incomingData);
-    } else {
-        return [];
-    }
-    const flattenedData: Array<FlattenedObject> = flattenObjects(dataToNormalize);
-    const cbifiedData = cbifyAll(flattenedData, normalizerConfig);
-    return cbifiedData;
-}
+
 export function cbifyData(input: Assets, normalizerConfig: NormalizerDeviceMap): Assets {
     const cbfiedData: Assets = {};
     Object.keys(normalizerConfig).forEach(function(value) {
-        //@ts-ignore
         cbfiedData[value] = input[normalizerConfig[value]];
-        //@ts-ignore
         delete input[normalizerConfig[value]];
     });
     cbfiedData.custom_data = {};
@@ -32,7 +18,6 @@ export function cbifyData(input: Assets, normalizerConfig: NormalizerDeviceMap):
         Object.keys(normalizerConfig.custom_data).forEach(function(value) {
             if (cbfiedData.custom_data) {
                 (cbfiedData.custom_data as { [key: string]: string })[value] =
-                    // @ts-ignore
                     input[normalizerConfig.custom_data[value]];
             }
         });
@@ -48,25 +33,15 @@ export function cbifyAll(input: Array<FlattenedObject>, normalizerConfig: Normal
     return cbfiedData;
 }
 
-export function flattenObjects(objArr: Array<Record<string, any>>): Array<FlattenedObject> {
-    const flattenedData: Array<FlattenedObject> = [];
-    for (let i = 0, l = objArr.length; i < l; i++) {
-        flattenedData.push(flattenJSON(objArr[i]));
-    }
-    return flattenedData;
-}
+export function flattenJSON(data: Record<string, unknown>): FlattenedObject {
+    const result: FlattenedObject = {};
 
-export function flattenJSON(data: Record<string, any>): FlattenedObject {
-    const result = {};
-
-    function recurse(cur: any, prop: string) {
+    function recurse(cur: Record<string, unknown>, prop: string): void {
         if (Object(cur) !== cur) {
-            //@ts-ignore
             result[prop] = cur;
         } else if (Array.isArray(cur)) {
-            for (var i = 0, l = cur.length; i < l; i++) recurse(cur[i], prop ? prop + '.' + i : '' + i);
-            if (l == 0) {
-                //@ts-ignore
+            for (let i = 0, l = cur.length; i < l; i++) recurse(cur[i], prop ? prop + '.' + i : '' + i);
+            if (cur.length === 0) {
                 result[prop] = [];
             }
         } else {
@@ -75,13 +50,33 @@ export function flattenJSON(data: Record<string, any>): FlattenedObject {
                 isEmpty = false;
                 recurse(cur[p], prop ? prop + '.' + p : p);
             }
-            if (isEmpty)
-                //@ts-ignore
-                result[prop] = {};
+            if (isEmpty) result[prop] = {};
         }
     }
     recurse(data, '');
     return result;
+}
+
+export function flattenObjects(objArr: Array<Record<string, unknown>>): Array<FlattenedObject> {
+    const flattenedData: Array<FlattenedObject> = [];
+    for (let i = 0, l = objArr.length; i < l; i++) {
+        flattenedData.push(flattenJSON(objArr[i]));
+    }
+    return flattenedData;
+}
+
+export function normalizeData(incomingData: unknown, normalizerConfig: NormalizerDeviceMap): Array<Assets> {
+    let dataToNormalize: Array<Record<string, unknown>> = [];
+    if (incomingData instanceof Array) {
+        dataToNormalize = incomingData;
+    } else if (typeof incomingData === 'object') {
+        dataToNormalize.push(incomingData);
+    } else {
+        return [];
+    }
+    const flattenedData: Array<FlattenedObject> = flattenObjects(dataToNormalize);
+    const cbifiedData = cbifyAll(flattenedData, normalizerConfig);
+    return cbifiedData;
 }
 
 export function cbFormatMacAddress(macAddr: string): string {
@@ -90,15 +85,15 @@ export function cbFormatMacAddress(macAddr: string): string {
 }
 
 export const Topics = {
-    AssetLocation: (ASSETID: string) => `_monitor/_asset/${ASSETID}/location`,
-    RulesAssetLocation: (ASSETID: string) => `_rules/_monitor/_asset/${ASSETID}/location`,
-    DBUpdateAssetLocation: (ASSETID: string) => `_dbupdate/_monitor/_asset/${ASSETID}/location`,
-    HistoryAssetLocation: (ASSETID: string) => `_history/_monitor/_asset/${ASSETID}/location`,
-    AssetHistory: (ASSETID: string) => `_history/_monitor/_asset/${ASSETID}/location`,
-    DBUpdateAssetStatus: (ASSETID: string) => `_dbupdate/_monitor/_asset/${ASSETID}/status`,
-    AreaLocationEvent: (AREAID: string) => `_monitor/_area/${AREAID}/location`,
-    ListenAllAssetsLocation: () => `_monitor/_asset/+/location`,
-    ListenAllAssetsStatus: () => `_monitor/_asset/+/status`,
+    AssetLocation: (ASSETID: string): string => `_monitor/_asset/${ASSETID}/location`,
+    RulesAssetLocation: (ASSETID: string): string => `_rules/_monitor/_asset/${ASSETID}/location`,
+    DBUpdateAssetLocation: (ASSETID: string): string => `_dbupdate/_monitor/_asset/${ASSETID}/location`,
+    HistoryAssetLocation: (ASSETID: string): string => `_history/_monitor/_asset/${ASSETID}/location`,
+    AssetHistory: (ASSETID: string): string => `_history/_monitor/_asset/${ASSETID}/location`,
+    DBUpdateAssetStatus: (ASSETID: string): string => `_dbupdate/_monitor/_asset/${ASSETID}/status`,
+    AreaLocationEvent: (AREAID: string): string => `_monitor/_area/${AREAID}/location`,
+    ListenAllAssetsLocation: (): string => `_monitor/_asset/+/location`,
+    ListenAllAssetsStatus: (): string => `_monitor/_asset/+/status`,
 };
 
 export function getAssetIdFromTopic(topic: string): string {
@@ -109,18 +104,18 @@ export function getAssetIdFromTopic(topic: string): string {
     return splitTopic[5];
 }
 
+export function isEmpty(str: string): boolean {
+    return !str || 0 === str.length;
+}
+
 export function isNormalizedDataValid(normalizedData: Array<Assets>): boolean {
     if (!(normalizedData instanceof Array) || normalizedData.length == 0) {
         return false;
     }
     for (let i = 0, l = normalizedData.length; i < l; i++) {
-        if (isEmpty(normalizedData[i]['id'])) {
+        if (isEmpty(normalizedData[i]['id'] as string)) {
             return false;
         }
     }
     return true;
-}
-
-export function isEmpty(str: any): boolean {
-    return !str || 0 === str.length;
 }
