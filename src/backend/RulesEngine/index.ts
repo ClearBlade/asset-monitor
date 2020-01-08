db31e5f393cf7eb0f28dbe0ee84a94a3eca4b695
Adam Alcott <aalcott14@gmail.com>
2020-01-08 11:06:23 -0600

    edit and delete rule methods

--- src/backend/RulesEngine/index.ts
+++ src/backend/RulesEngine/index.ts
@@ -1,6 +1,6 @@
 import '../../static/promise-polyfill';
 import 'core-js/features/map';
-import { Engine, Event, RuleProperties, TopLevelCondition, Almanac, AllConditions, AnyConditions, NestedCondition, ConditionProperties, RuleResult } from 'json-rules-engine';
+import { Engine, Event, RuleProperties, TopLevelCondition, Almanac, AllConditions, AnyConditions, NestedCondition, ConditionProperties, RuleResult, Rule } from 'json-rules-engine';
 import { StateParams } from './types';
 import { parseAndConvertConditions } from './convert-rule';
 import { doesTimeframeMatchRule } from './timeframe';
@@ -10,10 +10,11 @@ import { Rules } from '../collection-schema/Rules';
 import { CbCollectionLib } from '../collection-lib';
 import { Areas } from '../collection-schema/Areas';
 import { Asset } from '../collection-schema/Assets';
+import { thisExpression } from '@babel/types';
 
 export class RulesEngine {
     engine: Engine;
-    data: object;
+    rules: {[id: string]: Rule}; // track rules here since there is not getRule method needed for edit/delete
     constructor() {
         Number.parseFloat = parseFloat;
         const options = {
@@ -21,19 +22,32 @@ export class RulesEngine {
         };
         this.engine = new Engine([], options)
             .addFact('state', function(params, almanac) {
-                handleStateRule(params as StateParams, almanac);
+                handleStateCondition(params as StateParams, almanac);
             })
             .on('success', function(event, almanac, ruleResult) {
                 handleRuleSuccess(event, almanac, ruleResult)
             })
-        this.data = {};
+        this.rules = {};
     }
 
-    addRule(rule: RuleProperties): void {
-        this.engine.addRule(rule);
+    addRule(ruleData: Rules): void {
+        this.convertRule(ruleData).then((rule) => {
+            this.rules[rule.name] = rule;
+            this.engine.addRule(rule);
+        })
     }
 
-    async convertRule(ruleData: Rules): Promise<RuleProperties> {
+    editRule(id: string, ruleData: Rules): void {
+        this.deleteRule(id);
+        this.addRule(ruleData);
+    }
+
+    deleteRule(id: string): void {
+        delete this.rules[id];
+        this.engine.removeRule(this.rules[id])
+    }
+
+    async convertRule(ruleData: Rules): Promise<Rule> {
         const { label, event_type_id, priority, severity, id, timeframe, action_ids, conditions } = ruleData;
         
         const parsedConditions = JSON.parse(conditions || '{}');
@@ -42,7 +56,7 @@ export class RulesEngine {
 
         const promise = parseAndConvertConditions(id, parsedConditions).then(
             (convertedConditions: TopLevelCondition) => {
-                return {
+                return new Rule({
                     name: id,
                     conditions: convertedConditions,
                     event: {
@@ -57,7 +71,7 @@ export class RulesEngine {
                             ruleName: label,
                         },
                     },
-                };
+                });
             },
         );
         Promise.runQueue();
@@ -86,7 +100,7 @@ function handleRuleSuccess(event: Event, almanac: Almanac, ruleResult: RuleResul
     }
 }
 
-function handleStateRule(params: StateParams, almanac: Almanac) {
+function handleStateCondition(params: StateParams, almanac: Almanac) {
     const promise = almanac.factValue('incomingData').then((incomingData: any) => {
         const promise = almanac.factValue(params.id).then((customData) => customData).catch(() => {
             return new Promise((res) => { // custom data has not been fetched for asset
