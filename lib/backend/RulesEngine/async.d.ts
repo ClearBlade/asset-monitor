c34d95e68dc27b73f9612a5ac8ed53379840af2b
Adam Alcott <aalcott14@gmail.com>
2020-01-10 10:31:25 -0600

    add builds

--- lib/backend/RulesEngine/async.d.ts
+++ lib/backend/RulesEngine/async.d.ts
@@ -4,7 +4,11 @@ import { Actions } from '../collection-schema/Actions';
 export declare function getAllAssetsForType(assetType: string): Promise<Array<CbServer.CollectionSchema<Asset>>>;
 export declare function getAllAreasForType(areaType: string): Promise<Array<CbServer.CollectionSchema<Areas>>>;
 export declare function getActionByID(actionID: string): Promise<Actions>;
-export declare function getOpenStateForEvent(eventTypeId: string): Promise<string>;
+export interface EventState {
+    is_open: boolean;
+    state: string;
+}
+export declare function getStateForEvent(eventTypeId: string): Promise<EventState>;
 export declare function createEvent(item: Record<string, string | boolean | number>): Promise<{
     item_id: string;
 }[]>;
