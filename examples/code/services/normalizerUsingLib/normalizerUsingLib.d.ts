2890dfe6e076dfcb226b06866c2a8e4f3c1bc780
Spencer Means <hspencermeans@gmail.com>
2020-02-10 14:55:26 -0600

    Using @clearblade/code-service-types (#19)
    * Now using code-service-types; removed local types
    
    * Added code-service-types to files

--- examples/code/services/normalizerUsingLib/normalizerUsingLib.d.ts
+++ examples/code/services/normalizerUsingLib/normalizerUsingLib.d.ts
@@ -1,2 +1,2 @@
-/// <reference types="clearbladejs-server" />
+/// <reference types="@clearblade/code-service-types" />
 export declare function normalizerUsingLib(req: CbServer.BasicReq, resp: CbServer.Resp): void;
