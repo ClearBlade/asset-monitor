fa52cd6f0bf22758d72cfd04cfe515d74643aed4
Clark Bynum <cbynum@clearblade.com>
2020-02-26 10:57:50 -0600

    built new edge stuff

--- lib/backend/edge/edge-disconnected/index.d.ts
+++ lib/backend/edge/edge-disconnected/index.d.ts
@@ -1,7 +1,2 @@
-interface EdgeDisconnectedConfig {
-    req: CbServer.BasicReq;
-    resp: CbServer.Resp;
-    cacheName?: string;
-}
-declare function edgeDisconnected({ req, resp, cacheName }: EdgeDisconnectedConfig): void;
+import edgeDisconnected from '@clearblade/one-way-sync/edge/edge-disconnected';
 export default edgeDisconnected;
