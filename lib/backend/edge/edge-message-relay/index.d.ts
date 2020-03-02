6f16848cbcf5a7474231ddc55171a4e3dd85bf7e
Clark Bynum <cbynum@clearblade.com>
2020-03-02 10:33:05 -0600

    build

--- lib/backend/edge/edge-message-relay/index.d.ts
+++ lib/backend/edge/edge-message-relay/index.d.ts
@@ -1,10 +1,11 @@
-declare const _default: ({ edgeShouldRelayAssetHistory, edgeShouldRelayAssetStatus, edgeShouldRelayLocation, edgeShouldRelayRules, ...rest }: {
+declare const _default: ({ edgeShouldRelayAssetHistory, edgeShouldRelayAssetStatus, edgeShouldRelayLocation, edgeShouldRelayRules, topics, ...rest }: {
     req: CbServer.BasicReq;
     resp: CbServer.Resp;
     edgeShouldRelayLocation: boolean;
     edgeShouldRelayAssetStatus: boolean;
     edgeShouldRelayAssetHistory: boolean;
     edgeShouldRelayRules: boolean;
+    topics: string[];
     cacheName?: string | undefined;
     collectionName?: string | undefined;
 }) => void;
