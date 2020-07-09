e3253aad1739cb2fb2ce60010fb715cea6f7a5ac
Clark Bynum <sh3nan1gans@users.noreply.github.com>
2020-07-09 16:43:38 -0500

    External rules backend (#31)
    * update rulesEngine to call processEvent directly if rule is external
    
    * export type; nest external rule stuff under meta property in msg payload
    
    * add util function for publishing external events to the rules topic
    
    * log warning if we couldn't find an external rule for external event message

--- lib/backend/RulesEngine/events.d.ts
+++ lib/backend/RulesEngine/events.d.ts
@@ -2,4 +2,4 @@ import { RuleParams, Entities, WithParsedCustomData } from './types';
 import '@clearblade/promise-polyfill';
 import { EventSchema } from '../collection-schema/Events';
 export declare function processSuccessfulEvent(ids: string[], ruleParams: RuleParams, entities: Entities, actionTopic: string, trigger: WithParsedCustomData): void;
-export declare function processEvent(ruleParams: RuleParams, entities: Entities, actionTopic: string, trigger: WithParsedCustomData): Promise<EventSchema | void>;
+export declare function processEvent(ruleParams: Omit<RuleParams, 'ruleType' | 'ruleName'>, entities: Entities, actionTopic: string, trigger: WithParsedCustomData): Promise<EventSchema | void>;
